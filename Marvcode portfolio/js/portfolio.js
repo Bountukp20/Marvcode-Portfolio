da789dd5c4c8c9ce082f51bec048ee336869d701
M&B Code <105244471+Bountukp20@users.noreply.github.com>
2022-08-25 14:53:33 -0500

    Delete Marvcode portfolio directory

--- Marvcode portfolio/js/portfolio.js
+++
@@ -1,9 +0,0 @@
-let bcrypt = require('bcrypt')
-let password = "MarvieCodie"
-let password1 = "Marviecoolio"
-
-bcrypt.genSalt(10, (err, salt) => {
-    bcrypt.hash(password, salt, (err, hash) => {
-        console.log(hash);
-    })
-})
